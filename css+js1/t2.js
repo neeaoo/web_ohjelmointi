d110544c33f98fe7a6d5f0cf5c729554c16495ef
neeaoo <32387385+neeaoo@users.noreply.github.com>
2018-11-13 11:58:19 +0200

    css+js1
    tehty

---
+++ css+js1/t2.js
@@ -0,0 +1,25 @@
+document.getElementById('berry').style.backgroundColor ="red";
+const mansikka = document.getElementById('berry').innerHTML;
+console.log('I found the berry: ' + mansikka);
+
+document.querySelector('li[data-foodtype = squishy]').style.backgroundColor="orange";
+const appelsiini = document.querySelector('li[data-foodtype = squishy]').innerHTML;
+console.log('I found the berry: ' + appelsiini);
+
+const lista = document.getElementsByTagName('li');
+console.log(lista);
+
+for(let i = 0; i < lista.length; i++) {
+  lista[3].style.backgroundColor = 'green';
+  lista[i].style.width = '100px';
+  lista[i].style.listStyleType = 'none';
+  lista[i].style.border = '1px solid black';
+  console.log('Using the for loop here: ' + lista);
+}
+
+const nodeLista = document.querySelectorAll('li');
+console.log(nodeLista);
+
+[].forEach.call(nodeLista, function (item){
+  console.log(item);
+});
